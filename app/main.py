f22bcb7fe5e72b4ed0e00f757665f650648d84ae
zachdirk <zacharydirk@gmail.com>
2018-03-03 14:15:35 -0800

    test

--- app/main.py
+++ app/main.py
@@ -2,11 +2,25 @@ import bottle
 import os
 import random
 
+class GameBoard:
+    def __init__(self, id, width, height):
+        self.id = id
+        self.width = width
+        self.height = height
+                
+    def parse(self, data):
+        self.data = data
+        self.snakes = self.data["snakes"]["data"]
+        self.me = self.data["you"]
 
+    def direction(self):
+        
+        
 
+G = None
 @bottle.route('/')
 def static():
-    return "the server is running"
+    return "the server is is running"
 
 
 @bottle.route('/static/<path:path>')
@@ -20,7 +34,8 @@ def start():
     game_id = data.get('game_id')
     board_width = data.get('width')
     board_height = data.get('height')
-
+    global G
+    G = GameBoard(game_id, board_width, board_height)
     head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
@@ -38,9 +53,9 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
+    G.data = data
+    G.parse(data)
     # TODO: Do things with data
-    
     directions = ['up', 'down', 'left', 'right']
     direction = random.choice(directions)
     print direction
@@ -56,6 +71,6 @@ application = bottle.default_app()
 if __name__ == '__main__':
     bottle.run(
         application,
-        host=os.getenv('IP', '0.0.0.0'),
+        host=os.getenv('IP', '192.168.96.168'),
         port=os.getenv('PORT', '8080'),
         debug = True)
